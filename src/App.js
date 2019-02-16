42027c253c907cc3aef504b132e8196fd3748afd
Cory Schimmoeller <1609schimmoeller23@gmail.com>
2019-02-15 22:39:20 -0500

    Moved the add mood form to its own file and refactored it to hooks :)

--- src/App.js
+++ src/App.js
@@ -1,97 +1,15 @@
 import React, { Component } from "react";
-import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
+import Amplify from "aws-amplify";
 import awsmobile from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react";
-import * as queries from "./graphql/queries";
-import * as mutations from "./graphql/mutations";
-import styled from "styled-components";
-
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var yyyy = today.getFullYear();
-
-if (dd < 10) {
-  dd = "0" + dd;
-}
-
-if (mm < 10) {
-  mm = "0" + mm;
-}
-
-today = mm + "/" + dd + "/" + yyyy;
+import MoodForm from "./newEditMood";
 
 Amplify.configure(awsmobile);
 
-const FormContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { NewMoodTitle: "", NewMoodType: "" };
-
-    this.handleChange = this.handleChange.bind(this);
-    //this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(value, name) {
-    this.setState(state => {
-      return (state[name] = value);
-    });
-  }
-
-  async getUserValues() {
-    let allMoods = await API.graphql(graphqlOperation(queries.listMoodItems));
-    console.log("moods", allMoods);
-
-    let currentuser = await Auth.currentAuthenticatedUser();
-    console.log("user", currentuser.attributes);
-  }
-
   render() {
-    this.getUserValues();
-    return (
-      <div className="App">
-        <header className="App-header">
-          <FormContainer>
-            <div>Enter Today's Mood: </div>
-            <span>Mood Type</span>
-            <input
-              onChange={e => {
-                this.handleChange(e.target.value, "NewMoodTitle");
-              }}
-              value={this.state.NewMoodTitle}
-            />
-            <span>Notes:</span>
-            <input
-              onChange={e => {
-                this.handleChange(e.target.value, "NewMoodType");
-              }}
-              value={this.state.NewMoodType}
-            />
-            <button
-              onClick={async () => {
-                var moodDetails = {
-                  date: today,
-                  note: this.state.NewMoodTitle,
-                  mood: this.state.NewMoodType
-                };
-                await API.graphql(
-                  graphqlOperation(mutations.createMoodItem, {
-                    input: moodDetails
-                  })
-                );
-              }}
-            >
-              add
-            </button>
-          </FormContainer>
-        </header>
-      </div >
-    );
+    //this.getUserValues();
+    return <MoodForm />;
   }
 }
 
---
+++ src/newEditMood.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import * as queries from "./graphql/queries";
+import * as mutations from "./graphql/mutations";
+import styled from "styled-components";
+import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = "0" + dd;
+}
+
+if (mm < 10) {
+  mm = "0" + mm;
+}
+
+today = mm + "/" + dd + "/" + yyyy;
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+function MoodForm() {
+  // Declare a new state variable, which we'll call "count"
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
+
+  async function getUserValues() {
+    let allMoods = await API.graphql(graphqlOperation(queries.listMoodItems));
+    console.log("moods", allMoods);
+
+    let currentuser = await Auth.currentAuthenticatedUser();
+    console.log("user", currentuser.attributes);
+  }
+
+  getUserValues();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FormContainer>
+          <div>Enter Today's Mood: </div>
+          <span>Mood Type</span>
+          <input
+            onChange={e => {
+              setType(e.target.value);
+            }}
+            value={type}
+          />
+          <span>Notes:</span>
+          <input
+            onChange={e => {
+              setTitle(e.target.value);
+            }}
+            value={title}
+          />
+          <button
+            onClick={async () => {
+              var moodDetails = {
+                date: today,
+                note: this.state.NewMoodTitle,
+                mood: this.state.NewMoodType
+              };
+              await API.graphql(
+                graphqlOperation(mutations.createMoodItem, {
+                  input: moodDetails
+                })
+              );
+            }}
+          >
+            add
+          </button>
+        </FormContainer>
+      </header>
+    </div>
+  );
+}
+
+export default MoodForm;
